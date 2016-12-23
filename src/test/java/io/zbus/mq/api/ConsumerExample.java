3682495b335a06cdc5e4fa4b647ec5d5a0d45277
rushmore <44194462@qq.com>
2016-12-23 15:41:20 +1300

    update mq.api

--- src/test/java/io/zbus/mq/api/ConsumerExample.java
+++ src/test/java/io/zbus/mq/api/ConsumerExample.java
@@ -11,45 +11,7 @@ public class ConsumerExample {
 		
 		@SuppressWarnings("resource")
 		final MqClient client = new TcpMqClient("localhost:8080", ioDriver);  
-		client.configAuth(new Auth());
-		
-		client.consume(new ConsumeHandler() { 
-			@Override
-			public String topic() { 
-				return "MyTopic";
-			}
-			@Override
-			public String consumeGroup() {
-				return null;
-			}
-			
-			@Override
-			public int maxInFlightMessage() {
-				return 20;
-			}
-			
-			@Override
-			public void onQuit(MqClient client, Message message) { 
-				
-			}
-			
-			@Override
-			public void onMessage(MqClient client, Message message) {
-				System.out.println(message); 
-				client.ack(message);
-			}
-			
-			@Override
-			public void onConsumeOk(MqClient client, Message message) {
-				
-			}
-			
-			@Override
-			public void onConsumeCancelOk(MqClient client, Message message) {
-				
-			} 
-			
-		});
+		client.configAuth(new Auth()); 
 		 
 	}
 
